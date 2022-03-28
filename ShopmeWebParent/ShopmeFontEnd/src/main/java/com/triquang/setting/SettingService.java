f7d5aa372025d822d5b8da147b9f7c688d899e11
Tri Quang <triquang.15qt@gmail.com>
2022-03-28 13:30:54 +0700

    Apply General Settings for Shopping Application

---
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/triquang/setting/SettingService.java
@@ -0,0 +1,20 @@
+package com.triquang.setting;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.triquang.common.entity.Setting;
+import com.triquang.common.entity.SettingCategory;
+
+@Service
+public class SettingService {
+	@Autowired
+	private SettingRepository repo;
+
+	public List<Setting> getGeneralSettings() {
+		return repo.findByTwoCategories(SettingCategory.GENERAL, SettingCategory.CURRENCY);
+	}
+
+}
