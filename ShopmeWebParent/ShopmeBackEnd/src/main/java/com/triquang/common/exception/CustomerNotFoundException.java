13bf9a561b2e163a6c6ac478e7a589d93eaf0b8f
Tri Quang <triquang.15qt@gmail.com>
2022-04-10 21:15:34 +0700

    Code Customer Module

---
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/triquang/common/exception/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.triquang.common.exception;
+
+public class CustomerNotFoundException extends Exception {
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+}
--- ShopmeWebParent/ShopmeFontEnd/src/main/java/com/triquang/customer/CustomerService.java
+++ ShopmeWebParent/ShopmeFontEnd/src/main/java/com/triquang/customer/CustomerService.java
@@ -3,6 +3,8 @@ package com.triquang.customer;
 import java.util.Date;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,35 +16,52 @@ import com.triquang.setting.CountryRepository;
 import net.bytebuddy.utility.RandomString;
 
 @Service
+@Transactional
 public class CustomerService {
-	@Autowired private CountryRepository countryRepository;
-	@Autowired private CustomerRepository customerRepository;
-	@Autowired PasswordEncoder passwordEncoder;
-	
+	@Autowired
+	private CountryRepository countryRepository;
+	@Autowired
+	private CustomerRepository customerRepository;
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
 	public List<Country> listAllCountries() {
 		return countryRepository.findAllByOrderByNameAsc();
 	}
-	
+
 	public boolean isEmailUnique(String email) {
 		Customer customer = customerRepository.findByEmail(email);
-		
+
 		return customer == null;
 	}
-	
+
 	public void registerCustomer(Customer customer) {
 		encodePassword(customer);
 		customer.setEnabled(false);
 		customer.setCreatedTime(new Date());
-		
+
 		String randomCode = RandomString.make(64);
 		customer.setVerificationCode(randomCode);
-		
+
 		customerRepository.save(customer);
 	}
 
 	private void encodePassword(Customer customer) {
 		String encodePassword = passwordEncoder.encode(customer.getPassword());
 		customer.setPassword(encodePassword);
+
+	}
+
+	public boolean verify(String verificationCode) {
+		Customer customer = customerRepository.findByVerificationCode(verificationCode);
+
+		if (customer == null || customer.isEnabled()) {
+			return false;
+		} else {
+			customerRepository.enable(customer.getId());
+			return true;
+		}
+
 		
 	}
 }
